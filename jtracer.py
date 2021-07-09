80d3276842689835442b443d3e390214a258436f
Jeremy Tammik <jeremy.tammik@autodesk.com>
2021-07-09 13:06:34 +0200

    query kwh data

--- jtracer.py
+++ jtracer.py
@@ -19,22 +19,22 @@ PV_CURRENT = 0x3101
 BATT_SOC = 0x311A # state of charge, the percentage of battery remaining capacity
 BATT_VOLTAGE_MAX_DAY = 0x3302 # Maximum battery voltage today
 BATT_VOLTAGE_MIN_DAY = 0x3303 # Minimum battery voltage today
-x = 0x3304 # Consumed energy today L
-x = 0x3305 # Consumed energy today H
-x = 0x3306 # Consumed energy this month L
-x = 0x3307 # Consumed energy this month H
-x = 0x3308 # Consumed energy this year L
-x = 0x3309 # Consumed energy this year H
-x = 0x330A # Total consumed energy L
-x = 0x330B # Total consumed energy H
-x = 0x330C # Generated energy today L
-x = 0x330D # Generated energy today H
-x = 0x330E # Generated energy this month L
-x = 0x330F # Generated energy this month H
-x = 0x3310 # Generated energy this year L
-x = 0x3311 # Generated energy this year H
-x = 0x3312 # Total generated energy L
-x = 0x3313 # Total generated energy H
+KWH_CONSUMED_DAY_L = 0x3304 # Consumed energy today L
+KWH_CONSUMED_DAY_H = 0x3305 # Consumed energy today H
+KWH_CONSUMED_MONTH_L = 0x3306 # Consumed energy this month L
+KWH_CONSUMED_MONTH_H = 0x3307 # Consumed energy this month H
+KWH_CONSUMED_YEAR_L = 0x3308 # Consumed energy this year L
+KWH_CONSUMED_YEAR_H = 0x3309 # Consumed energy this year H
+KWH_CONSUMED_TOTAL_L = 0x330A # Total consumed energy L
+KWH_CONSUMED_TOTAL_H = 0x330B # Total consumed energy H
+KWH_DAY_L = 0x330C # Generated energy today L
+KWH_DAY_H = 0x330D # Generated energy today H
+KWH_MONTH_L = 0x330E # Generated energy this month L
+KWH_MONTH_H = 0x330F # Generated energy this month H
+KWH_YEAR_L = 0x3310 # Generated energy this year L
+KWH_YEAR_H = 0x3311 # Generated energy this year H
+KWH_TOTAL_L = 0x3312 # Total generated energy L
+KWH_TOTAL_H = 0x3313 # Total generated energy H
 BATT_VOLTAGE = 0x331A # Battery voltage
 BATT_CURRENT_L = 0x331B # Battery current L
 BATT_CURRENT_H = 0x331C # Battery current H
@@ -79,18 +79,54 @@ instrument = setParameters( '/dev/tty.SLAB_USBtoUART', baudrate )
 
 if instrument:
 
+  t = strftime('%Y-%m-%d %H:%M:%S', gmtime())
+  
+  pv_voltage = readint( PV_VOLTAGE )
+  pv_current = readint( PV_CURRENT )
+  batt_voltage = readint( BATT_VOLTAGE )
+  batt_current_l = readint( BATT_CURRENT_L )
+  batt_current_h = readint( BATT_CURRENT_H )
+  batt_soc = readint( BATT_SOC )
+  batt_voltage_max_day = readint( BATT_VOLTAGE_MAX_DAY )
+  batt_voltage_min_day = readint( BATT_VOLTAGE_MIN_DAY )
+  kwh_day_l = readint( KWH_DAY_L )
+  kwh_day_h = readint( KWH_DAY_H )
+  kwh_month_l = readint( KWH_MONTH_L )
+  kwh_month_h = readint( KWH_MONTH_H )
+  kwh_year_l = readint( KWH_YEAR_L )
+  kwh_year_h = readint( KWH_YEAR_H )
+  kwh_total_l = readint( KWH_TOTAL_L )
+  kwh_total_h = readint( KWH_TOTAL_H )
+  kwh_consumed_day_l = readint( KWH_CONSUMED_DAY_L )
+  kwh_consumed_day_h = readint( KWH_CONSUMED_DAY_H )
+  kwh_consumed_month_l = readint( KWH_CONSUMED_MONTH_L )
+  kwh_consumed_month_h = readint( KWH_CONSUMED_MONTH_H )
+  kwh_consumed_year_l = readint( KWH_CONSUMED_YEAR_L )
+  kwh_consumed_year_h = readint( KWH_CONSUMED_YEAR_H )
+  kwh_consumed_total_l = readint( KWH_CONSUMED_TOTAL_L )
+  kwh_consumed_total_h = readint( KWH_CONSUMED_TOTAL_H )  
+
+  print t
+  print 'PV:', pv_voltage, 'V', pv_current, 'A', pv_voltage * pv_current, 'W', 
+  print 'Battery:', batt_voltage, 'V', batt_current_l, 'A', batt_voltage * batt_current_l, 'W', batt_soc, '%'
+  print 'Day Min', batt_voltage_min_day, 'V, Max', batt_voltage_max_day, 'V'
+  print 'kWh day/month/year/total', kwh_day_l, kwh_month_l, kwh_year_l, kwh_total_l
+  print 'Consumed kWh day/month/year/total', kwh_consumed_day_l, kwh_consumed_month_l, kwh_consumed_year_l, kwh_consumed_total_l
+
   # Loop forever
   
   while (1):
     t = strftime('%Y-%m-%d %H:%M:%S', gmtime())
-    print t, 'PV Voltage', readint( PV_VOLTAGE )
-    print t, 'PV Current', readint( PV_CURRENT )
-    print t, 'Battery Voltage', readint( BATT_VOLTAGE ), 'V'
-    print t, 'Battery Current', readint( BATT_CURRENT_L ), 'A'
-    #print t, 'Battery Current H', readint( BATT_CURRENT_H )
-    print t, 'Percent charged', readint( BATT_SOC ), '%'
-    print t, 'Day Voltage Min', readint( BATT_VOLTAGE_MIN_DAY ), 'V'
-    print t, 'Day Voltage Max', readint( BATT_VOLTAGE_MAX_DAY ), 'V'
+    
+    pv_voltage = readint( PV_VOLTAGE )
+    pv_current = readint( PV_CURRENT )
+    batt_voltage = readint( BATT_VOLTAGE )
+    batt_current_l = readint( BATT_CURRENT_L )
+    #batt_current_h = readint( BATT_CURRENT_H )
+    batt_soc = readint( BATT_SOC )
+  
+    print t, 'PV:', pv_voltage, 'V', pv_current, 'A', pv_voltage * pv_current, 'W', 
+    print 'Battery:', batt_voltage, 'V', batt_current_l, 'A', batt_voltage * batt_current_l, 'W', batt_soc, '%'
   
     break
   
